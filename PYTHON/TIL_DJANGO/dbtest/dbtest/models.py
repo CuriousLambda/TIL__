b5b6508ad955176f6f1f900b2f6812924f387955
CuriousLambda <hyuna3589@gmail.com>
2022-10-23 20:26:15 +0900

    DJANGO3

--- PYTHON/TIL_DJANGO/dbtest/dbtest/models.py
+++ PYTHON/TIL_DJANGO/dbtest/dbtest/models.py
@@ -7,3 +7,17 @@ class MyBoard(models.Model):
     mycontent = models.CharField(max_length = 2000)
     mydate = models.DateField()
 
+    # 원하는 문자열의 형태로 보여주기.
+    # java 에서 toString과 비슷하다.
+    def __str__(self):
+        return str({"myname" : self.myname, "mytitle" : self.mytitle, "mycontent" : self.mycontent, "mydate" : self.mydate})
+
+
+
+class MyMember(models.Model):
+    myname = models.CharField(max_length = 100)
+    mypassword = models.CharField(max_length = 100)
+    myemail = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str({"myname" : self.myname, "mypassword" : self.mypassword, "myemail" : self.myemail})
